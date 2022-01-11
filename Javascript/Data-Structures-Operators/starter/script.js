a0e81f6d139df28db602beeb3fa7054775af869c
Marian Adamus <cmanar0@gmail.com>
2022-01-11 13:26:19 +0100

    Practice Short Circuiting and Nullish operator

--- Javascript/Data-Structures-Operators/starter/script.js
+++ Javascript/Data-Structures-Operators/starter/script.js
@@ -147,6 +147,7 @@ const restaurant = {
 // // -------------------- PRACTICE DESCRUCTURING OBJECTS - END -------------------
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // // -------------------- PRACTICE THE SPREAD OPERATOR - START -------------------
+
 // // 1) We can use SPREAD OPERATOR to access all it`s values. So basically unpacking all the array values at one.
 // const arr = [7, 8, 9]
 // // if we want to expand this array with some other elements, normaly we would have to do it like this for example:
@@ -229,3 +230,54 @@ const restaurant = {
 
 // restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach')
 // restaurant.orderPizza('mushrooms')
+
+// // -------------------- PRACTICE THE SPREAD OPERATOR - END -------------------
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+// // -------------------- PRACTICE SHORT CIRCUITING - START -------------------
+
+// Short Circuiting (&& and ||)
+
+console.log('---- OR ----')
+// Use ANY data type, return ANY data type, short-circuiting
+console.log(3 || 'Jonas')
+console.log('' || 'Jonas')
+console.log(true || 0)
+console.log(undefined || null)
+
+console.log(undefined || 0 || '' || 'Hello' || 23 || null)
+
+restaurant.numGuests = 0
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10
+console.log(guests1)
+
+const guests2 = restaurant.numGuests || 10
+console.log(guests2)
+
+console.log('---- AND ----')
+console.log(0 && 'Jonas')
+console.log(7 && 'Jonas')
+
+console.log('Hello' && 23 && null && 'jonas')
+
+// Practical example
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('mushrooms', 'spinach')
+}
+
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach')
+// // -------------------- PRACTICE SHORT CIRCUITING - END -------------------
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+// // -------------------- NULLISH OPERATOR - START -------------------
+
+// The Nullish Coalescing Operator
+restaurant.numGuests = 0
+const guests = restaurant.numGuests || 10
+console.log(guests)
+
+// Nullish: null and undefined (NOT 0 or '')
+const guestCorrect = restaurant.numGuests ?? 10
+console.log(guestCorrect)
